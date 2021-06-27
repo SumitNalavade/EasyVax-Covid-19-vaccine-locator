86250a843895130be71f8cea487bd586bf67351b
Sumit Nalavade <48499839+SumitNalavade@users.noreply.github.com>
2021-06-27 02:27:35 -0500

    fix search placeholder and icon

--- flaskapp.py
+++ flaskapp.py
@@ -17,7 +17,7 @@ def send():
         city = request.form['city']
         state = request.form['state']
 
-        home_location = streetaddress + ", " + city + ", " + state
+        home_location = f'{streetaddress}, {city}, {state}'
 
         API_KEY = "AIzaSyDVKqdtzC0GmyYJBcjLCpbea_BMZ58lo4k"
 
@@ -31,38 +31,45 @@ def send():
             latitude = response['results'][0]['geometry']['location']['lat']
             longitude = response['results'][0]['geometry']['location']['lng']
 
-        response = map_client.places(query=location_name, location=(latitude,longitude))
+        response = map_client.places(
+            query=location_name, location=(latitude, longitude))
 
-        name_array = []
-        address_array = []
-        photo_ref_array = []  
-        phone_numbers_array = []
-        website_array = []
+        location_array = []
+
+        class location():
+            def __init__(self, name, address, photo, phone_number, website):
+                self.name = name
+                self.address = address
+                self.photo = photo
+                self.phone_number = phone_number
+                self.website = website
 
         for i in range(6):
-            name_array.append((response["results"][i]["name"]))
-            address_array.append((response["results"][i]["formatted_address"]))
+            name = (response["results"][i]["name"])
+            address = (response["results"][i]["formatted_address"])
 
             try:
-                photo_ref = ((response["results"][i]["photos"][0]['photo_reference']))
+                photo_ref = (
+                    (response["results"][i]["photos"][0]['photo_reference']))
                 img_url = f"https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference={photo_ref}&sensor=false&key=AIzaSyDVKqdtzC0GmyYJBcjLCpbea_BMZ58lo4k"
-                photo_ref_array.append(img_url)
+                photo = (img_url)
             except:
-                photo_ref_array.append("https://149362684.v2.pressablecdn.com/wp-content/uploads/COVID-Vaccine-Graphic.png")
+                photo = (
+                    "https://149362684.v2.pressablecdn.com/wp-content/uploads/COVID-Vaccine-Graphic.png")
 
-
-        for i in range(6):
             my_place_id = response['results'][i]['place_id']
             my_fields = ['formatted_phone_number', 'website']
 
-            phone_numbers_array.append((map_client.place(place_id=my_place_id, fields=my_fields))['result']['formatted_phone_number'])
+            phone_number = ((map_client.place(place_id=my_place_id, fields=my_fields))[
+                            'result']['formatted_phone_number'])
 
             try:
-                website_array.append((map_client.place(place_id=my_place_id, fields=my_fields))['result']['website'])
+                website = (map_client.place(place_id=my_place_id, fields=my_fields))[
+                    'result']['website']
             except:
-                "https://covvax.herokuapp.com/"
+                website = "https://covvax.herokuapp.com/"
 
-    
-        return(render_template('index.html', address_array=address_array, name_array=name_array, photo_ref_array=photo_ref_array, phone_numbers_array=phone_numbers_array, website_array=website_array))
+            location_array.append(
+                location(name, address, photo, phone_number, website))
 
-
+        return(render_template('index.html', location_array=location_array))
