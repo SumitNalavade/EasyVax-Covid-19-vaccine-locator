ae83ab2d1633ed4db8f30b06ead4ea721935d82b
Sumit Nalavade <48499839+SumitNalavade@users.noreply.github.com>
2021-07-25 02:13:18 -0500

    add link open on image click

--- static/js/script.js
+++ static/js/script.js
@@ -3,12 +3,12 @@ function openLinkOnImageClick() {
     document.querySelectorAll(".card-text a").forEach((site) => sitesArr.push(site));
 
     for(let i = 0; i < sitesArr.length; i++) {
-        document.querySelectorAll(".card-img-top")[i].addEventListener("click", () => window.location.assign(sitesArr[i]));
+        document.querySelectorAll(".card-img-top")[i].addEventListener("click", () => window.open(sitesArr[i]));
     }
 }
 
 function goToHomeOnLogoClick() {
-    document.querySelector(".logoimg").addEventListener("click", () => window.location.assign("https://easyvax.herokuapp.com"));
+    document.querySelector(".logoimg").addEventListener("click", () => window.location.href = "https://easyvax.herokuapp.com/")
 }
 
 openLinkOnImageClick();
